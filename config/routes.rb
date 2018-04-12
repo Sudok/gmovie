5360709f0fc11918f229ab262572acbac0c38c63
sudok <glauber.gw@gmail.com>
2018-04-11 23:29:59 -0300

    creating admins controller and setting admins views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   get 'backoffice', to: 'backoffice/dashboard#index'
-  get 'admin', to: 'backoffice/dashboard#index'
+  # get 'admin', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
     resources :categories, except: [:show, :destroy]
     get 'dashboard', to: 'dashboard#index'
+    get 'admins/index'
   end
 
   namespace :site do
