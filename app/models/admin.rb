9fae6a771270fa8da9ba2751f929d6b017c4dcac
sudok <glauber.gw@gmail.com>
2018-04-18 22:37:24 -0300

    ajusting model admin and setting radio buttons on partial from admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -13,12 +13,12 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def role_description
-    if self.role == 'full_access'
-      'Acesso Completo'
-    else
-      "Acesso Restrito"
-    end
-  end
+  # def role_description
+  #   if self.role == 'full_access'
+  #     'Acesso Completo'
+  #   else
+  #     "Acesso Restrito"
+  #   end
+  # end
 
 end
